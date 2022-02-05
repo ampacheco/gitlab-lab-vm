741b2865408e7d5ab91dcd0559d648e90bd283ca
ampacheco <ampacheco@users.noreply.github.com>
2022-02-05 16:41:52 -0500

    :clapper: docker-command-created

---
+++ run-docker.sh
@@ -0,0 +1,13 @@
+sudo docker run \
+--detach \
+--hostname <your-host-name> \
+--publish 443:443 \
+--publish 80:80 \
+--publish 22:22 \
+--name gitlab \
+--restart always \
+--volume \
+/srv/gitlab/config:/etc/gitlab \
+--volume /srv/gitlab/logs:/var/log/gitlab \
+--volume /srv/gitlab/data:/var/opt/gitlab \  
+gitlab/gitlab-ce:latest
