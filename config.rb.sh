b39ee7376af24b867505daacee31644acfcda925
ampacheco <ampacheco@users.noreply.github.com>
2022-02-07 00:19:28 -0500

    config files added

--- config.rb.sh
+++ config.rb.sh
@@ -1,6 +1,3 @@
-sudo cat<<EOF >>  /srv/gitlab/config/gitlab.rb
-# My Domain Parameters
-registry_external_url 'https://registry.XXXXXXXXXXXX.io'
-external_url 'https://gitlab.XXXXXXXXXXX.io'
-letsencrypt['enable'] = true
-EOF
+echo "registry_external_url 'https://registry.XXXXXXXXXXXX.io'" | sudo teaa -a /srv/gitlab/config/gitlab.rb
+echo "external_url 'https://gitlab.XXXXXXXXXXX.io'" | sudo teaa -a /srv/gitlab/config/gitlab.rb
+echo "letsencrypt['enable'] = true'" | sudo teaa -a /srv/gitlab/config/gitlab.rb
